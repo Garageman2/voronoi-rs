bed3af8a8d64a2176451a3af00aa84e411d56e10
Bailey <61953092+Garageman2@users.noreply.github.com>
2023-01-20 21:54:35 -0800

    Randomness Added

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 extern crate image;
 extern crate gif;
-extern crate core;
 use std::collections::VecDeque;
 use image::{Rgb, RgbImage, Pixel};
 use gif::{Encoder};
+use rand::Rng;
+
 
 fn draw_line(img: &mut RgbImage,p1:(u32,u32),p2:(u32,u32),col:[u8;3])
 {
     let RCol = Rgb(col);
     if p2.0 == p1.0
     {
-        println!("strike");
         //handles a vertical
         for y in p1.1..p2.1
         {
@@ -72,14 +72,9 @@ fn draw_voronoi2(x:u32,y:u32,col:[u8;3],img: &mut RgbImage, l:u32,w:u32, points:
 
 fn draw_voronoi(x:u32,y:u32,col:[u8;3],img: &mut RgbImage, l:u32,w:u32, points: &mut VecDeque<((u32,u32),[u8;3])>)
 {
-    println!("{},{}",x,y);
     if img.get_pixel(x,y) == &Rgb([0,0,0])
     {
         img.put_pixel(x,y,Rgb(col));
-
-        //111
-        //1x1
-        //111
 
         if ((x as i32) -1 >= 0) && ((y as i32) -1 >= 0)
         {
@@ -125,6 +120,15 @@ fn draw_voronoi(x:u32,y:u32,col:[u8;3],img: &mut RgbImage, l:u32,w:u32, points: 
     }
 }
 
+fn gen_seeds(count:u16,height:u32,width:u32,points:&mut VecDeque<((u32,u32),[u8;3])>)
+{
+    for _i in 0..count
+    {
+        let mut rng = rand::thread_rng();
+        points.push_back(((rng.gen_range(0..width),rng.gen_range(0..height)),[rng.gen_range(0..255),rng.gen_range(0..255),rng.gen_range(0..255)]));
+    }
+}
+
 fn main() {
     const HEIGHT: u32 = 256;
     const WIDTH: u32 = 256;
@@ -136,16 +140,11 @@ fn main() {
 
     //submit a point and a color for the point suggested.
     let mut points:VecDeque<((u32,u32),[u8;3])> = VecDeque::new();
-    points.push_front(((1,1),[0,0,255]));
-    points.push_front(((128,128),[0,255,0]));
-    points.push_front(((255,255),[100,100,0]));
-    points.push_front(((40,100),[0,0,100]));
-    points.push_front(((170,90),[160,0,120]));
-    points.push_front(((80,210),[90,10,200]));
+    gen_seeds(5,HEIGHT,WIDTH,&mut points);
     while points.len() > 0
     {
         let point = points.pop_front().unwrap();
-        draw_voronoi2(point.0.0,point.0.1,point.1, &mut img, HEIGHT, WIDTH, &mut points);
+        draw_voronoi(point.0.0,point.0.1,point.1, &mut img, HEIGHT, WIDTH, &mut points);
     }
 
     img.save("images/output.png");
