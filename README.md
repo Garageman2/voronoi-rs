9f99b3344190aefc1c8db0158c924b63ddb860ff
Bailey <61953092+Garageman2@users.noreply.github.com>
2023-01-24 23:04:48 -0800

    Drew Proper cells

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # voronoi-rs
 
 A project to generate manhattan voronoi diagrams quickly through flood filling from the seeds outward rather than calculating distance.
-!["An example of a diagram generated"](images/output.png)
+!["An example of a diagram generated"](images/Demo.png)
--- src/main.rs
+++ src/main.rs
@@ -173,6 +173,55 @@ fn draw_voronoi_with_lines(x:u32,y:u32,col:[u8;3],img: &mut RgbImage, l:u32,w:u3
     }
 }
 
+fn draw_bands(img: &mut RgbImage, l:u32,w:u32, points: &mut VecDeque<((u32,u32),[u8;3])>)
+{
+    for y in 0..l
+    {
+        for x in 0..w
+        {
+            let mut Nearest:(f32,[u8;3]) = (1000000.0,[255,255,255]);
+            for Seed in points.iter()
+            {
+                let dist: f32 = ((y as f32 - Seed.0.0 as f32).powf(2.0) + (x as f32 - Seed.0.0 as f32).powf(2.0)).sqrt();
+                if dist < Nearest.0
+                {
+                    Nearest = (dist,Seed.1);
+                }
+            }
+            img.put_pixel(x,y,Rgb(Nearest.1));
+
+
+        }
+    }
+}
+
+fn draw_cells(img: &mut RgbImage, l:u32,w:u32, points: &mut VecDeque<((u32,u32),[u8;3])>, lines:bool)
+{
+    for y in 0..l
+    {
+        for x in 0..w
+        {
+            let mut Nearest:(f32,[u8;3]) = (1000000.0,[255,255,255]);
+            for Seed in points.iter()
+            {
+                let mut dist:f32 = ( (y as f32 - Seed.0.1 as f32).powi(2) + (x as f32 - Seed.0.0 as f32).powi(2)).sqrt();
+                if (Nearest.0.floor() - dist.floor()).abs() <= 0.75 && lines
+                {
+                    Nearest = (dist,[255,255,255]);
+                }
+                else if dist < Nearest.0
+                {
+                    Nearest = (dist,Seed.1);
+                }
+            }
+            img.put_pixel(x,y,Rgb(Nearest.1));
+
+
+        }
+    }
+}
+
+
 fn gen_seeds(count:u16,height:u32,width:u32,points:&mut VecDeque<((u32,u32),[u8;3])>)
 {
     for _i in 0..count
@@ -185,18 +234,19 @@ fn gen_seeds(count:u16,height:u32,width:u32,points:&mut VecDeque<((u32,u32),[u8;
 fn main() {
     const HEIGHT: u32 = 2160;
     const WIDTH: u32 = 3840;
-    const SEEDS:u16 = 150;
+    const SEEDS:u16 = 128;
     let mut img:RgbImage = RgbImage::new(WIDTH,HEIGHT);
 
     //submit a point and a color for the point suggested.
     let mut points:VecDeque<((u32,u32),[u8;3])> = VecDeque::new();
     gen_seeds(SEEDS,HEIGHT,WIDTH,&mut points);
-    while points.len() > 0
-    {
-        let point = points.pop_front().unwrap();
-        draw_voronoi_with_lines(point.0.0,point.0.1,point.1, &mut img, HEIGHT, WIDTH, &mut points);
-    }
+    draw_bands(&mut img, HEIGHT, WIDTH, &mut points);
+    // while points.len() > 0
+    // {
+    //     let point = points.pop_front().unwrap();
+    //     draw_voronoi_with_lines(point.0.0,point.0.1,point.1, &mut img, HEIGHT, WIDTH, &mut points);
+    // }
 
-    img.save("images/output.png").expect("failed to save");
+    img.save("images/Demo4.png").expect("failed to save");
 
 }
